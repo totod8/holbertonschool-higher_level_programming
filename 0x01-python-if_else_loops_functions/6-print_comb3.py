a6ffc0f9c35427f07ed68b7bd7b266ae8f1c786f
totod8 <totod8@hotmail.com>
2022-01-12 18:52:26 -0500

    mycommit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,4 @@ for n in range(0, 8):
     num_two = n + 1
     for t in range(num_two, 10):
         print("{:d}{:d}".format(n, t), end=", ")
-        print("{}".format("89"))
+print("{}".format("89"))
