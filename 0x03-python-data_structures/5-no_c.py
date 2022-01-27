e156201ef37422b465e4e0b7229e485833cfe20a
totod8 <totod8@hotmail.com>
2022-01-27 15:02:08 -0500

    mycommit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_str = ""
-    for i in my_string:
-        if i is not 'c' and i is not 'C':
-            new_str += i
-    return (new_str)
+    # Create a string empty
+    string_two = ""
+    len_my_string = len(my_string)
+    for c in range(0, len(my_string)):
+        # If the condition is met, fill in the empty string created
+        if my_string[c] != 'c' and my_string[c] != 'C':
+            string_two += my_string[c]
+    return string_two
