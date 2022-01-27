4d102eb8349e3af89fc7231cc7a22815c31482b7
totod8 <totod8@hotmail.com>
2022-01-27 16:26:35 -0500

    mycommit

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list == []:
+        return None
+    # Initially the first number in the list is the largest
+    big_number = my_list[0]
+    for n in my_list:
+        # It goes through and compares each number, if it finds that is greater
+        # than the one assigned in position 0 of the list, it automatically
+        # assigns the number that meets that condition, the value of big_number
+        if n > big_number:
+            big_number = n
+        return big_number
